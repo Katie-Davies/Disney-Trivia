e7ab5f51d8b2f92a5fe763ae32fd86e3fe0e46a7
Katie Davies <millerkatie1990@gmail.com>
2024-04-29 18:39:44 +1200

    styling leadership board

--- client/components/Leadership.tsx
+++ client/components/Leadership.tsx
@@ -1,5 +1,40 @@
+import { useGetLeaders } from '../hooks/useGetLeaderboard'
+
 function Leadership() {
-  return <h1>Leadership</h1>
+  const { data: leaderboard, isLoading, isError } = useGetLeaders()
+  if (isLoading) {
+    return <p>Loading ...</p>
+  }
+  if (isError) {
+    return <p>There has been an error!</p>
+  }
+  const sortedLeadershipBoard = leaderboard?.sort((a, b) => b.score - a.score)
+
+  if (leaderboard)
+    return (
+      <div>
+        <h1>Leadership</h1>
+
+        {sortedLeadershipBoard?.map((player) => {
+          return (
+            <>
+              <div className="scoreboard">
+                <div className="scoreboard-div">
+                  <p key={player.id}>{player.name}</p>
+                </div>
+                <div className="scoreboard-div">
+                  <p>{player.score}</p>
+                </div>
+              </div>
+            </>
+          )
+        })}
+
+        {/* {leaderboard.map((player) => {
+            return <td key={player.id}>{player.score}</td>
+          })} */}
+      </div>
+    )
 }
 
 export default Leadership
--- client/hooks/useGetLeaderboard.ts
+++ client/hooks/useGetLeaderboard.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 
 import { getLeadershipBoard } from '../apis/leadership'
 
-export function useGetQuestions() {
+export function useGetLeaders() {
   return useQuery({
     queryKey: ['Leaderboard'],
     queryFn: () => getLeadershipBoard(),
