572b8fd67316c3c318f70d37ecc15013eee18be4
Katie Davies <millerkatie1990@gmail.com>
2024-04-30 15:55:54 +1200

    Working query test

--- client/components/Leadership.test.tsx
+++ client/components/Leadership.test.tsx
@@ -1,6 +1,11 @@
 //@vitest-environment jsdom
 import { describe, it, expect } from 'vitest'
-import { screen, waitFor, within } from '@testing-library/react'
+import {
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+} from '@testing-library/react'
 import { renderApp } from '../test-utils'
 import nock from 'nock'
 
@@ -26,5 +31,38 @@ describe('leadership', () => {
     const loading = await waitFor(() => screen.getByText(/loading/i))
     expect(loading).toBeVisible()
     expect(scope.isDone()).toBe(true)
+  })
+
+  it('show players', async () => {
+    //ARRANGE
+    const scope = nock('http://localhost')
+      .get('/api/v1/leadership')
+      .reply(200, [
+        {
+          id: 1,
+          name: 'Katie',
+          score: 3,
+        },
+        {
+          id: 2,
+          name: 'Buddy',
+          score: 5,
+        },
+      ])
+
+    renderApp('/leadershipboard')
+    const paragraph = await waitFor(() => screen.getByText('Katie'))
+    expect(paragraph).toBeVisible()
+
+    expect(scope.isDone()).toBe(true)
+  })
+  it('shows error message', async () => {
+    //ARRANGE
+    const scope = nock('http://localhost').get('/api/v1/leadership').reply(500)
+    //ACT
+    renderApp('/leadershipboard')
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
+    const errorMsg = screen.getByText('There has been an error!')
+    expect(errorMsg).toBeInTheDocument()
   })
 })
